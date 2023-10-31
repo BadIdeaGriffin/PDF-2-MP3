ef6a2f5ccd4e5864491d9a1db97f35a4961d46a9
BadIdeaGriffin <chrisgriffin333@gmail.com>
2023-10-31 11:06:24 -0700

    Minor indent push

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ language = 'en'
 text = ""
 #Extract text. will need to replace pdf for each new PDF
 with reader.open('DICOM_CookBook.pdf') as pdf:
-    for page in pdf.pages:
-        text += page.extract_text()
+    with reader.askopenfilename() as pdf:
+        for page in pdf.pages:
+            text += page.extract_text()
         print(text)
 pdf.close()
 
 #conversion of text to mp3.
 #will need to replace MP3 for each new job
-speaker.save_to_file(text, 'DICOM_CookBook.mp3')
+"""speaker.save_to_file(text, 'DICOM_CookBook.mp3')
 speaker.runAndWait()
 speaker.stop()
-print("WORK DONE")
+print("WORK DONE")"""
